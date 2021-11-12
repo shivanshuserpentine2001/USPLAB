e0f77912794370c0ba7f91f8793734a66fa5e0e9
shivanshuserpentine2001 <63629636+shivanshuserpentine2001@users.noreply.github.com>
2021-11-12 10:37:07 +0530

    Create lab2prog2.sh

---
+++ lab2prog2.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+n=0
+i=0
+sum=0
+echo "ENTER UPTO WHICH NO";
+read n
+while [ $i -le $n ] 
+do
+        if [ `expr $i % 2` -eq 0 ]
+        then
+          sum=`expr $sum + $i`;
+                i=`expr $i + 1`;
+        else
+                i=`expr $i + 1`;
+
+        fi
+done
+    
